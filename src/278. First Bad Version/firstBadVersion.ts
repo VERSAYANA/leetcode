15223d414e13dfa5f97988efc29ccd93a8bf983b
Ramtin Khalatbari <VERSAYANA@outlook.com>
2021-09-28 12:24:31 +0330

    Add a bit better solution for 278

--- src/278. First Bad Version/firstBadVersion.ts
+++ src/278. First Bad Version/firstBadVersion.ts
@@ -5,13 +5,32 @@
  * };
  */
 
-var solution = function (isBadVersion: any) {
+// Time complexity: O(log n)
+// Space complexity: O(1)
+const solution1 = function (isBadVersion: any) {
+  return function (n: number): number {
+    let start = 1;
+    let end = n;
+
+    while (start < end) {
+      const mid = Math.floor(start + (end - start) / 2);
+      if (isBadVersion(mid)) {
+        end = mid;
+      } else {
+        start = mid + 1;
+      }
+    }
+    return start;
+  };
+};
+
+const solution2 = function (isBadVersion: any) {
   return function (n: number): number {
     let start = 0;
     let end = n - 1;
 
     while (start <= end) {
-      const current = Math.floor((end + start) / 2);
+      const current = Math.floor(start + (end - start) / 2);
       if (!isBadVersion(current)) {
         if (isBadVersion(current + 1)) {
           return current + 1;
