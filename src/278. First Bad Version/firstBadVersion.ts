ea2b2cfc659d3a0b0eba91805f8e48f52f8b046d
Ramtin Khalatbari <VERSAYANA@outlook.com>
2021-09-28 12:30:29 +0330

    Swap solutions for 278

--- src/278. First Bad Version/firstBadVersion.ts
+++ src/278. First Bad Version/firstBadVersion.ts
@@ -9,25 +9,6 @@
 // Space complexity: O(1)
 const solution1 = function (isBadVersion: any) {
   return function (n: number): number {
-    let start = 1;
-    let end = n;
-
-    while (start < end) {
-      const mid = Math.floor(start + (end - start) / 2);
-      if (isBadVersion(mid)) {
-        end = mid;
-      } else {
-        start = mid + 1;
-      }
-    }
-    return start;
-  };
-};
-
-// Time complexity: O(log n)
-// Space complexity: O(1)
-const solution2 = function (isBadVersion: any) {
-  return function (n: number): number {
     let start = 0;
     let end = n - 1;
 
@@ -48,5 +29,24 @@ const solution2 = function (isBadVersion: any) {
       }
     }
     return 0;
+  };
+};
+
+// Time complexity: O(log n)
+// Space complexity: O(1)
+const solution2 = function (isBadVersion: any) {
+  return function (n: number): number {
+    let start = 1;
+    let end = n;
+
+    while (start < end) {
+      const mid = Math.floor(start + (end - start) / 2);
+      if (isBadVersion(mid)) {
+        end = mid;
+      } else {
+        start = mid + 1;
+      }
+    }
+    return start;
   };
 };
